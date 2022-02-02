ac4049b9368a8103bc5da98e6defd5f286ed22e0
crosschainer <eodkwokm@gmail.com>
2022-02-02 20:42:42 +0100

    init

---
+++ js/wallet.js
@@ -0,0 +1,38 @@
+// Requires jQuery
+var address = "";
+
+
+// 1. Defining dApp Connection
+const detail = JSON.stringify({
+    appName: 'NFT Wallet',
+    version: '0.0.1',
+    logo: 'logo.png',
+    contractName: '',
+    networkType: 'mainnet',
+})
+
+// 2. Wallet Event Listener
+document.addEventListener('lamdenWalletInfo', (response) => {
+    if (response.detail.errors === undefined) {
+        //Wallet is connected
+        
+    }
+});
+
+// 2. TX Event Listener
+document.addEventListener('lamdenWalletTxStatus', (response) => {
+    //Listens to Transactions
+});
+
+// 3. Dispatch Event on Page Load
+$(document).ready(function () {
+    // Get Wallet if already connected
+    document.dispatchEvent(new CustomEvent('lamdenWalletGetInfo'));
+});
+
+// 4. Connect a Wallet Function
+$("#connect_wallet").click(function () {
+    console.log("Connect your wallet has been triggered.");
+    document.dispatchEvent(new CustomEvent('lamdenWalletConnect', { detail }));
+});
+
